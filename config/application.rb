10d4dec4329d39ca2e74f4f440dfa411ad313ae4
pseudomuto <david.muto@gmail.com>
2014-02-21 03:57:14 +0000

    configuring ember variants for dev, test and prod

--- config/application.rb
+++ config/application.rb
@@ -19,5 +19,8 @@ module Emberrails
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+
+    # use development by default
+    config.ember.variant = :development
   end
 end
