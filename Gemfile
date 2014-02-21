9cff689238d475583827af4828f7a78b6903882e
pseudomuto <david.muto@gmail.com>
2014-02-21 03:54:30 +0000

    initial commit

---
+++ Gemfile
@@ -0,0 +1,17 @@
+source 'https://rubygems.org'
+
+# Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
+gem 'rails', '4.0.3'
+
+# Use sqlite3 as the database for Active Record
+gem 'sqlite3'
+gem 'jquery-rails'
+gem 'ember-rails'
+gem 'ember-source'
+
+group :assets do
+  gem 'sass-rails', '~> 4.0.0'
+  gem 'uglifier', '>= 1.3.0'
+  gem 'coffee-rails', '~> 4.0.0'
+  gem 'ember_script-rails', github: 'ghempton/ember-script-rails'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Emberrails::Application.config.secret_key_base = '5cfde1c58c4dd079235aef0a00f0594b683551cdea6e0e8baafd69aa78d171ef90382429a82c3def29c4ee02bfb4d19c422cc77f1b2fa7ed9c6a5175c73a676a'
